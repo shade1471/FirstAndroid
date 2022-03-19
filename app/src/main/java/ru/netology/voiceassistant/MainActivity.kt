391f8eebf6c6bed3703e0784065aca56625f008e
Andrey Gribanov <shade1471@gmail.com>
2022-03-19 18:16:04 +0700

    first lesson

--- app/src/main/java/ru/netology/voiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/voiceassistant/MainActivity.kt
@@ -3,28 +3,95 @@ package ru.netology.voiceassistant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.SimpleAdapter
 import android.widget.TextView
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    val TAG: String = "MainActivity"
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        Log.d(TAG, "start of onCreate function")
+    val TAG: String = "MainActivity"
+
+    lateinit var requestInput: TextInputEditText
+
+    lateinit var podsAdapter: SimpleAdapter
+
+    lateinit var progressBar: ProgressBar
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title","Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title","Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String, String>().apply {
+            put("Title","Title 3")
+            put("Content", "Content 3")
+        },
+        HashMap<String, String>().apply {
+            put("Title","Title 4")
+            put("Content", "Content 4")
+        }
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val name: String = "Ivan"
-        val surname: String = "Ivanov"
-        var age: Int = 37
-        var height: Double = 172.2
+        initViews()
+    }
 
-        val summary: String = "name: $name surname: $surname age: $age height: $height"
-        val output: TextView = findViewById(R.id.output)
-        output.text = summary
+    fun initViews() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        Log.d(TAG, summary)
+        requestInput = findViewById(R.id.text_input_edit)
 
-        Log.d(TAG, "end of onCreate function")
+        val podsList: ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Content"),
+            intArrayOf(R.id.title, R.id.content)
+        )
+        podsList.adapter = podsAdapter
+
+        val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
+        voiceInputButton.setOnClickListener {
+            Log.d(TAG, "FAB")
+        }
+
+        progressBar= findViewById(R.id.progress_bar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "action_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "action_clear")
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
