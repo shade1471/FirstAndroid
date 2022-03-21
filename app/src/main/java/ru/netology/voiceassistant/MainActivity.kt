8f8df5714b43a67d0c078b8e36bfb6cb65279e91
Andrey Gribanov <shade1471@gmail.com>
2022-03-21 17:35:43 +0700

    lesson 3

--- app/src/main/java/ru/netology/voiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/voiceassistant/MainActivity.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.ListView
 import android.widget.ProgressBar
 import android.widget.SimpleAdapter
 import android.widget.TextView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.wolfram.alpha.WAEngine
+import com.wolfram.alpha.WAPlainText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    // 8W5GRT-6PT72RW7TU
 
     val TAG: String = "MainActivity"
 
@@ -23,24 +34,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var progressBar: ProgressBar
 
-    val pods = mutableListOf<HashMap<String, String>>(
-        HashMap<String, String>().apply {
-            put("Title","Title 1")
-            put("Content", "Content 1")
-        },
-        HashMap<String, String>().apply {
-            put("Title","Title 2")
-            put("Content", "Content 2")
-        },
-        HashMap<String, String>().apply {
-            put("Title","Title 3")
-            put("Content", "Content 3")
-        },
-        HashMap<String, String>().apply {
-            put("Title","Title 4")
-            put("Content", "Content 4")
-        }
-    )
+    lateinit var waEngine: WAEngine
+
+    val pods = mutableListOf<HashMap<String, String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -48,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initViews()
+        initWolframEngine()
     }
 
     fun initViews() {
@@ -55,6 +52,16 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         requestInput = findViewById(R.id.text_input_edit)
+        requestInput.setOnEditorActionListener {v , actionId , event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                pods.clear()
+                podsAdapter.notifyDataSetChanged()
+
+                val question = requestInput.text.toString()
+                askWolfram(question)
+            }
+            return@setOnEditorActionListener false
+        }
 
         val podsList: ListView = findViewById(R.id.pods_list)
         podsAdapter = SimpleAdapter(
@@ -71,7 +78,8 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "FAB")
         }
 
-        progressBar= findViewById(R.id.progress_bar)
+        progressBar = findViewById(R.id.progress_bar)
+
 
     }
 
@@ -81,17 +89,83 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.action_stop -> {
                 Log.d(TAG, "action_stop")
                 return true
             }
             R.id.action_clear -> {
-                Log.d(TAG, "action_clear")
+                requestInput.text?.clear()
+                pods.clear()
+                podsAdapter.notifyDataSetChanged()
                 return true
             }
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    fun initWolframEngine() {
+        waEngine = WAEngine().apply {
+            appID = "8W5GRT-6PT72RW7TU"
+            addFormat("plaintext")
+        }
+    }
+
+    fun showSnackbar(message: String) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_INDEFINITE)
+            .apply {
+                setAction(android.R.string.ok) {
+                    dismiss()
+                }
+                show()
+            }
+    }
+
+    fun askWolfram(request: String) {
+        progressBar.visibility = View.VISIBLE
+        CoroutineScope(Dispatchers.IO).launch {
+            val query = waEngine.createQuery().apply { input = request }
+            runCatching {
+                waEngine.performQuery(query)
+            }.onSuccess { result ->
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.GONE
+                    if (result.isError) {
+                        showSnackbar(result.errorMessage)
+                        return@withContext
+                    }
+
+                    if (!result.isSuccess) {
+                        requestInput.error = getString(R.string.error_do_not_understand)
+                        return@withContext
+                    }
+
+                    for (pod in result.pods) {
+                        if (!pod.isError) continue
+                        val content = StringBuilder()
+                        for (subpod in pod.subpods) {
+                            for (element in subpod.contents) {
+                                if (element is WAPlainText) {
+                                    content.append(element.text)
+                                }
+                            }
+                        }
+                        pods.add(0, HashMap<String, String>().apply {
+                            put("Title", pod.title)
+                            put("Content", content.toString())
+                        })
+                    }
+                    podsAdapter.notifyDataSetChanged()
+                }
+
+            }.onFailure { t ->
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.GONE
+                    showSnackbar(t.message ?: getString(R.string.error_something_went_wrong))
+
+                }
+            }
+        }
     }
 }
